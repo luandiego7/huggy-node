678a6ae47f55c870aaaca08e8f995e62fd372720
unknown <luan@objetivosistemas.com.br>
2021-02-16 23:05:08 -0300

    puppeteer - readme

---
+++ README.md
@@ -0,0 +1,20 @@
+# Abrir navegador em página específica
+
+Sistema de Teste para abrir o navegador em uma determinada página
+
+## Start project
+
+Recomendações para execução do projeto:
+
+- Crie uma pasta chamada huggy-node.
+- Dentro da pasta, execute no terminal o comando `git clone https://github.com/luandiego7/huggy-node.git.
+- Em seguida, execute no terminal `npm start` , para iniciar o sevidor express.
+- Abra um programa da sua preferência que envie requisições, indico o postman, que foi utilizado para teste neste projeto.
+- Envie uma requisição post para a url `http://localhost:3000/alert`.
+- O projeto está setado para abrir o navegador utilizando child_process com spawn. Caso queira utilizar o puppeteer, execute no terminal `npm i puppeteer` para instalar o puppeteer.
+- Abra a pasta `routes`, e no arquivo `index.js` comente as linhas 13 a 27, e descomente as linhas 29 a 43.
+- Envie novamente a requisição post.
+
+Requisitos para execução do projeto:
+
+- NodeJs
--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,46 @@
-const express = require('express');
-const spawn = require('child_process').spawn;
-var exe = 'C:\\Program Files\\Google\\Chrome\\application\\chrome.exe';
+const express   = require('express');
+const Spawn     = require('child_process').spawn;
+const puppeteer = require('puppeteer');
 
 const router = express.Router();
 router.get('/', (req, res) => {
-    res.send('Olá mundo!');
+    res.send('Huggy!');
 });
 
+
 router.post('/alert', (req, res) => {
+
+    Spawn('google-chrome', [ 'https://web.whatsapp.com/',
+        //'--headless',
+        '--disable-gpu',
+        '--no-sandbox',
+        '--remote-debugging-port=9222',
+        '--remote-debugging-address=0.0.0.0',
+        '--user-data-dir=data',
+        '--no-first-run',
+        '--disable-popup-blocking',
+        '--disable-infobars',
+        '--start-maximized',
+        '--use-fake-device-for-media-stream',
+        '--mute-audio',
+        '--ignore-certificate-errors'
+    ]);
+
+    /*(async () => {
+        const browser = await puppeteer.launch({
+            headless:false,
+            defaultViewport:null
+        });
+        const page = await browser.newPage();
+        await page.goto('https://web.whatsapp.com/');
+        page.on('dialog', async dialog => {
+            console.log(dialog.message());
+            await dialog.dismiss();
+            //await browser.close();
+        });
+        page.evaluate(() => alert('huggy'));
+
+    })();*/
 
 
 });
